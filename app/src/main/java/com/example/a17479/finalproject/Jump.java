599709f5f9e6a47ff5d060114690f44019419b92
YouHeiHei <chensi2@illinois.edu>
2018-12-02 19:01:54 -0600

    Initial commit

---
+++ app/src/main/java/com/example/a17479/finalproject/Jump.java
@@ -0,0 +1,15 @@
+package com.example.a17479.finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class Jump extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jump);
+    }
+
+}
