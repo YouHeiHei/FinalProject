ae3884aba85b7118fb77b9f834e522caed6df30b
YouHeiHei <chensi2@illinois.edu>
2018-12-03 23:37:02 -0600

    Update2

--- app/src/main/java/com/example/a17479/finalproject/Jump.java
+++ app/src/main/java/com/example/a17479/finalproject/Jump.java
@@ -1,18 +1,21 @@
 package com.example.a17479.finalproject;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import java.net.URL;
 
 public class Jump extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_jump);
+        setContentView(R.layout.gaming_mode);
     }
 }
--- app/src/main/java/com/example/a17479/finalproject/Start.java
+++ app/src/main/java/com/example/a17479/finalproject/Start.java
@@ -1,15 +1,21 @@
 package com.example.a17479.finalproject;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import java.net.URL;
 
 public class Start extends AppCompatActivity {
-
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jump);
+    }
     protected void button(Button button) {
         final Button start_1 = findViewById(R.id.start_button1);
         final Button start_2 = findViewById(R.id.start_button2);
@@ -17,19 +23,27 @@ public class Start extends AppCompatActivity {
         start_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+//                URL url = new URL("http://image10.bizrate-images.com/resize?sq=60&uid=2216744464");
+//                Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+//                imageView.setImageBitmap(bmp);
                 startActivity(new Intent(Start.this, Jump.class));
             }
         });
         start_2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                URL url = new URL("http://image10.bizrate-images.com/resize?sq=60&uid=2216744464");
+//                Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+//                imageView.setImageBitmap(bmp);
                 startActivity(new Intent(Start.this, Jump.class));
             }
         });
         start_3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                URL url = new URL("http://image10.bizrate-images.com/resize?sq=60&uid=2216744464");
+//                Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+//                imageView.setImageBitmap(bmp);
                 startActivity(new Intent(Start.this, Jump.class));
             }
         });
