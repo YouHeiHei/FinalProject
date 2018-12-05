d57433ad80c29a96a11fe7a77ed60f22f432fdc6
YouHeiHei <chensi2@illinois.edu>
2018-12-05 17:15:26 -0600

    GameMode play role image set1

--- app/src/main/java/com/example/a17479/finalproject/Jump.java
+++ app/src/main/java/com/example/a17479/finalproject/Jump.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import java.net.URL;
 
@@ -17,5 +18,12 @@ public class Jump extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gaming_mode);
+        final ImageButton jump = findViewById(R.id.JumpButton);
+        jump.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/a17479/finalproject/Start.java
+++ app/src/main/java/com/example/a17479/finalproject/Start.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import java.net.URL;
 
@@ -15,36 +16,37 @@ public class Start extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jump);
-    }
-    protected void button(Button button) {
         final Button start_1 = findViewById(R.id.start_button1);
         final Button start_2 = findViewById(R.id.start_button2);
         final Button start_3 = findViewById(R.id.start_button3);
         start_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                URL url = new URL("http://image10.bizrate-images.com/resize?sq=60&uid=2216744464");
-//                Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-//                imageView.setImageBitmap(bmp);
-                startActivity(new Intent(Start.this, Jump.class));
+                Intent intent = new Intent(Start.this, Jump.class);
+                setContentView(R.layout.gaming_mode);
+                startActivity(intent);
+                ImageView role = (ImageView) findViewById(R.id.PlayRole);
+                role.setImageResource(R.drawable.dino1);
             }
         });
         start_2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                URL url = new URL("http://image10.bizrate-images.com/resize?sq=60&uid=2216744464");
-//                Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-//                imageView.setImageBitmap(bmp);
-                startActivity(new Intent(Start.this, Jump.class));
+                Intent intent = new Intent(Start.this, Jump.class);
+                setContentView(R.layout.gaming_mode);
+                startActivity(intent);
+                ImageView role = (ImageView) findViewById(R.id.PlayRole);
+                role.setImageResource(R.drawable.turtle1);
             }
         });
         start_3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                URL url = new URL("http://image10.bizrate-images.com/resize?sq=60&uid=2216744464");
-//                Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-//                imageView.setImageBitmap(bmp);
-                startActivity(new Intent(Start.this, Jump.class));
+                Intent intent = new Intent(Start.this, Jump.class);
+                setContentView(R.layout.gaming_mode);
+                startActivity(intent);
+                ImageView role = (ImageView) findViewById(R.id.PlayRole);
+                role.setImageResource(R.drawable.rabbit1);
             }
         });
     }
